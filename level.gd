00b0397063a1350ba0fa3fc7d6ae69eaa8e03152
Brian Kelly <polymonic@gmail.com>
2024-03-02 21:55:30 -0600

    Use scene paths instead of packed scenes to void cyclical dependency

--- level.gd
+++ level.gd
@@ -1,11 +1,13 @@
 class_name Level
 extends Node
 
-@export var next_level: PackedScene
+@export_file("*.tscn") var next_level
 
 func _on_ufo_health_depleted():
 	get_tree().reload_current_scene()
 
-
 func _on_mothership_completed():
-	get_tree().change_scene_to_packed(next_level)
+	call_deferred("load_level", next_level)
+
+func load_level(level_path:String):
+	get_tree().change_scene_to_file(level_path)
